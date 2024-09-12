1b11fe4e374e9dd23f299c1030c94b3b8d5dc2af
Przemyslaw Kalucki <pakalucki@gmail.com>
2024-09-12 10:42:49 +0200

    rotate log added

--- logrotate.go
+++ logrotate.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
@@ -34,7 +35,7 @@ func runLogRotation() {
 		if err != nil {
 			log.Printf("Failed to schedule task for path %s: %v", logEntry.Path, err)
 		} else {
-			log.Printf("Scheduled task for path %s with schedule: %s", logEntry.Path, schedule)
+			log.Printf("Scheduled task for path %s", logEntry.Path)
 		}
 	}
 
@@ -97,51 +98,75 @@ func createTask(logEntry LogEntry) func() {
 }
 
 func rotateLogFiles(logEntry LogEntry) {
-	log.Printf("Rotating logs for path: %s", logEntry.Path)
-	// matchingFiles, err := filepath.Glob(filepath.Clean(logEntry.Path))
-	// if err != nil {
-	// 	log.Printf("Failed to expand wildcard for path %s: %v", logEntry.Path, err)
-	// 	return
-	// }
+	for _, path := range logEntry.Path {
+		log.Printf("Rotating logs for path: %s", path)
+		matchingFiles, err := filepath.Glob(filepath.Clean(path))
+		if err != nil {
+			log.Printf("Failed to expand wildcard for path %s: %v", path, err)
+			return
+		}
 
-	// for _, file := range matchingFiles {
-	// 	// Check if file size exceeds the specified condition
-	// 	fileInfo, err := os.Stat(file)
-	// 	if err != nil {
-	// 		log.Printf("Failed to get file info for %s: %v", file, err)
-	// 		continue
-	// 	}
+		for _, file := range matchingFiles {
+			fileInfo, err := os.Stat(file)
+			if err != nil {
+				log.Printf("Failed to get file info for %s: %v", file, err)
+				continue
+			}
 
-	// 	// Parse size condition
-	// 	maxSize, err := parseSize(logEntry.Condition.Size)
-	// 	if err != nil {
-	// 		log.Printf("Invalid size format for rotation: %v", err)
-	// 		continue
-	// 	}
+			// Check the size condition, if provided
+			rotateDueToSize := false
+			if logEntry.Condition.Size != nil {
+				maxSize, err := parseSize(*logEntry.Condition.Size)
+				if err != nil {
+					log.Printf("Invalid size format for rotation: %v", err)
+					continue
+				}
 
-	// 	if fileInfo.Size() >= maxSize {
-	// 		rotatedFilePath := fmt.Sprintf("%s.%s", file, time.Now().Format("20060102-150405"))
-	// 		if err := os.Rename(file, rotatedFilePath); err != nil {
-	// 			log.Printf("Failed to rotate log file %s: %v", file, err)
-	// 			continue
-	// 		}
-	// 		log.Printf("Rotated log file: %s to %s", file, rotatedFilePath)
+				if fileInfo.Size() >= maxSize {
+					rotateDueToSize = true
+				}
+			}
 
-	// 		// Compress the rotated log file if necessary
-	// 		if logEntry.Condition.Compress == nil || *logEntry.Condition.Compress {
-	// 			if err := compressLogFile(rotatedFilePath); err != nil {
-	// 				log.Printf("Failed to compress rotated log file %s: %v", rotatedFilePath, err)
-	// 			} else {
-	// 				log.Printf("Compressed log file: %s", rotatedFilePath)
-	// 			}
-	// 		}
+			// Check the age condition, if provided
+			rotateDueToAge := false
+			if logEntry.Condition.Age != nil {
+				ageDuration, err := parseDuration(*logEntry.Condition.Age)
+				if err != nil {
+					log.Printf("Invalid age format for rotation: %v", err)
+					continue
+				}
 
-	// 		// Remove old log files if MaxKeep is set
-	// 		if logEntry.Condition.MaxKeep != nil {
-	// 			if err := removeOldLogFiles(filepath.Dir(file), filepath.Base(file), *logEntry.Condition.MaxKeep); err != nil {
-	// 				log.Printf("Failed to remove old log files: %v", err)
-	// 			}
-	// 		}
-	// 	}
-	// }
+				fileAge := time.Since(fileInfo.ModTime())
+				if fileAge >= ageDuration {
+					rotateDueToAge = true
+				}
+			}
+
+			// Rotate the file if either size or age condition is met
+			if rotateDueToSize || rotateDueToAge {
+				rotatedFilePath := fmt.Sprintf("%s.%s", file, time.Now().Format("20060102-150405"))
+				if err := os.Rename(file, rotatedFilePath); err != nil {
+					log.Printf("Failed to rotate log file %s: %v", file, err)
+					continue
+				}
+				log.Printf("Rotated log file: %s to %s", file, rotatedFilePath)
+
+				// Compress the rotated log file if necessary
+				if logEntry.Condition.Compress == nil || *logEntry.Condition.Compress {
+					if err := compressLogFile(rotatedFilePath); err != nil {
+						log.Printf("Failed to compress rotated log file %s: %v", rotatedFilePath, err)
+					} else {
+						log.Printf("Compressed log file: %s", rotatedFilePath)
+					}
+				}
+
+				// Remove old log files if MaxKeep is set
+				if logEntry.Condition.MaxKeep != nil {
+					if err := removeOldLogFiles(filepath.Dir(file), filepath.Base(file), *logEntry.Condition.MaxKeep); err != nil {
+						log.Printf("Failed to remove old log files: %v", err)
+					}
+				}
+			}
+		}
+	}
 }
